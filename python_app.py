16e076924ed06d06783ad627f9361d7c9931a826
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-09-13 13:02:50 +0300

    added the question with answer

---
+++ python_app.py
@@ -0,0 +1,17 @@
+# a dictionary that stores questions and answers
+# have a variable that traks the store of the player
+# loop through the dictionary using the key values pairs
+# display each question to the user and allow them to answer
+# tell them if they agree right or wrong
+# show the final result when quis is completed
+
+quiz = {
+    "question1" :{
+    "question" : "what is the capital of France?",
+    "answer" : "Paris"
+    },
+    "question2" :{
+    "question" : "what is the capital of Germany?",
+    "answer" : "Berlin"
+    },
+}
