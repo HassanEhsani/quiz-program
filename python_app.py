163752725dd43e91c6598f65aace9eb5d2748586
HassanEhsani <hassan.ehsani.am@gmail.com>
2024-09-13 13:33:16 +0300

    wrote the function about Dic

--- python_app.py
+++ python_app.py
@@ -14,4 +14,40 @@ quiz = {
     "question" : "what is the capital of Germany?",
     "answer" : "Berlin"
     },
+    "question3" :{
+    "question" : "what is the capital of Italy?",
+    "answer" : "Rome"
+    },
+    "question4" :{
+    "question" : "what is the capital of Spain?",
+    "answer" : "Madrid"
+    },
+    "question5" :{
+    "question" : "what is the capital of Portuagal?",
+    "answer" : "Lisbon"
+    },
+    "question6" :{
+    "question" : "what is the capital of Switzerland?",
+    "answer" : "Bern"
+    },
+    "question7" :{
+    "question" : "what is the capital of Austria?",
+    "answer" : "Vienna"
+    },
 }
+
+score = 0
+
+for key, value in quiz.items():
+    print(value['question'])
+    answer = input("Answer? ")
+    
+    if answer.lower() == value["answer"].lower():
+        print("Correct!")
+        score = score + 1
+        print("Your score is : " + str(score))
+        
+    else:
+        print("Wrong!")
+        print("The answer is : " + value ["answer"])
+        print("Your score is : " + str(score))
